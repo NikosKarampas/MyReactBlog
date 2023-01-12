b55a52a68249022a77bbc507a5c71113b7c7611c
Nikos Karampas <nikos.karampas@hotmail.com>
2023-01-12 15:02:22 +0200

    Added a form to add a new blog (Create.js)

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (  
         <nav className="navbar">
-            <h1>My React Blog</h1>
+            <Link to="/">
+                <h1>My React Blog</h1>
+            </Link> 
             <div className="links">
                 <Link className="nav-link nav-link-fade-up" to="/">Home</Link>
                 <Link className="nav-link nav-link-fade-up" to="/create">New Blog</Link>
